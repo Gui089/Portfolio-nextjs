59174a10da82e557e686dbbd16f889cc8ddae52e
Gui089 <guilhermesilba811@gmail.com>
2024-09-21 14:05:24 -0300

    feat: add page experience

---
+++ app/experiencia/page.tsx
@@ -0,0 +1,19 @@
+
+
+const Experience = () => {
+    return (
+        <div className="absolute w-full mt-[100px] flex justify-around">
+
+
+            <div className="w-[30%]">
+                <h1>Experiência de Trabalho</h1>
+            </div>
+
+            <div>
+                <h1>Minhas experiências coomo desenvolvedor:</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Experience;
