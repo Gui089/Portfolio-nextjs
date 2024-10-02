39cf8e13837fc114bb370d970f76f953e1fb8527
Gui089 <guilhermesilba811@gmail.com>
2024-10-02 16:18:53 -0300

    feat: add exp in component

--- app/experiencia/page.tsx
+++ app/experiencia/page.tsx
@@ -11,17 +11,37 @@ const Experience = () => {
     }, []);
 
     return (
-        <div className="absolute w-full mt-[130px] items-center flex justify-around">
+        <div className="absolute w-full mt-[100px] items-center flex justify-around">
 
 
-            <div data-aos="fade-up" className="w-[30%]">
+            <div data-aos="fade-up" className="w-[30%] mt-[90px]">
                 <h1 className="text-white text-2xl">Experiência de Trabalho</h1>
-                <h1 className='ml-[100px] mt-[100px]'>2024</h1>
+                <h1 className='ml-[100px] mt-[100px] font-semibold text-xl text-[#5de4a1]'>2024</h1>
+
+                <div className='w-full mt-[239px] bg-slate-400 h-[1px]'></div>
+
+                <h1 className='ml-[100px] mt-[100px] font-semibold text-xl text-[#5de4a1]'>2023 - 2024</h1>
+
             </div>
 
-            <div  data-aos="fade-up" className='mt-[-65px]'>
+            <div  data-aos="fade-up" className='mt-[-20px]'>
                 <h1 className='text-gray-500'>Minhas experiências coomo desenvolvedor:</h1>
+                <h1 className='text-gray-200 mt-[95px]'>SinergiaRH - Desenvolvedor Full Stack - React/.NET</h1>
+
+                <div className='w-[300px] flex items-center'>
+                    <p className='text-gray-400'>
+                        Sou responsável pela criação e manutenção 
+                        de aplicações em React/Nextjs no front-end e .NET no back-end, trabalho com testes end to end, testes unitários,
+                        com deploy na AWS via EC2, utilizando Docker para ambiente de desenvolvimento e banco de dados PostgreSQL e MySQL .
+                        </p>
+                </div>
+
+                <div className='w-full mt-[50px] bg-slate-400 h-[1px]'></div>
+
+                
             </div>
+
+            
         </div>
     )
 }
